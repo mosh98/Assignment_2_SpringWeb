538d27de0322803c75414948578d28e7878b01df
mosh98 <mosleh.edu@gmail.com>
2023-02-24 11:24:08 +0100

    application.properties and character fixed

---
+++ src/main/java/com/example/assignment_2_springweb/model/Character.java
@@ -0,0 +1,33 @@
+package com.example.assignment_2_springweb.model;
+
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter // Lombok
+@ToString // Lombok
+
+public class Character {
+    /**
+     * Character
+     * • Autoincremented Id
+     * • Full name
+     * • Alias (if applicable)
+     * • Gender
+     * • Picture (URL to photo – do not store an image)
+     */
+
+    private int id;
+    private String fullName;
+
+    private String Alias;
+
+    private String gender;
+
+    private String picture;
+
+    public Character() {
+    }
+
+
+}
