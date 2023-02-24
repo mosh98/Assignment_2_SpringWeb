5faf600d1bd26e5acfdcadedab283dfdcf48b8fa
mosh98 <mosleh.edu@gmail.com>
2023-02-24 09:23:22 +0100

    Initial commit

---
+++ src/main/java/com/example/assignment_2_springweb/Assignment2SpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_2_springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2SpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2SpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment_2_springweb/Assignment2SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_2_springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
