414465fc08ef3d5c6bc0a30c91f69ad0f0d04d74
mosh98 <mosleh.edu@gmail.com>
2023-02-27 14:09:45 +0100

    data and mappings DONE

---
+++ src/main/resources/data_movie_char.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie_characters (movie_id, character_id) VALUES (1,1);
+INSERT INTO movie_characters (movie_id, character_id) VALUES (1,2);
+INSERT INTO movie_characters (movie_id, character_id) VALUES (1,3);
+INSERT INTO movie_characters (movie_id, character_id) VALUES (2,3);
+INSERT INTO movie_characters (movie_id, character_id) VALUES (2,4);
