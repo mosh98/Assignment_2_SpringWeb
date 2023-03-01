3c833da7a0bda26e21e6371c6a00753e189ae05b
Silvia.A.A <silvia.alvarez.allende@se.experis.com>
2023-03-01 08:36:34 +0100

    Add: FranchiseRepository, FranchiseService, FranchiseServiceImp

---
+++ src/main/java/com/example/assignment_2_springweb/services/franchise/FranchiseService.java
@@ -0,0 +1,7 @@
+package com.example.assignment_2_springweb.services.franchise;
+
+import com.example.assignment_2_springweb.model.Franchise;
+import com.example.assignment_2_springweb.services.CrudService;
+
+public interface FranchiseService extends CrudService<Franchise, Integer> {
+}
